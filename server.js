b0d64476c18f6a816302b6c2856724095a47ca27
Joseph Browning <j.browning527@gmail.com>
2024-02-06 17:00:12 -0500

    fixed issue with not using express.json.  added id and helper function for uuid

--- server.js
+++ server.js
@@ -2,16 +2,20 @@ const express = require('express');
 const path = require('path');
 const noteData = require('./db/db.json');
 const fs = require('fs');
+const uuid = require('./helpers/uuid');
 
 const PORT = 3001;
 
 const app = express();
 
+
 app.use(express.static('public'));
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
+
 app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/notes.html'));
 });
@@ -26,23 +30,19 @@ app.get('/api/notes', (req, res) => {
 
 //Post request to add new notes
 app.post('/api/notes', (req, res) => {
-  
+  const newNote = { ...req.body, id: uuid() }
   fs.readFile(path.join(__dirname, './db/db.json'), (err, data) => {
     if (err) { console.log(err) }
-    const { title, text } = req.body;
-    const newNote = {
-      title,
-      text
-    }
+    
     const notes = JSON.parse(data);
     notes.push(newNote);
-    console.log(notes);
-  })
+
 
   fs.writeFile(path.join(__dirname, './db/db.json'), JSON.stringify(notes), (err) => {
     if (err) { console.log(err) }
     res.json(newNote);
   })
+})
 })
 
 

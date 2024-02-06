d9933f14825cb40156327d08830c0c931a5155d6
Joseph Browning <j.browning527@gmail.com>
2024-02-06 17:16:16 -0500

    fixing port issue for heroku

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const noteData = require('./db/db.json');
 const fs = require('fs');
 const uuid = require('./helpers/uuid');
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
